314722d25ed6e8d081dca1fb901e321bff4b5429
pheonix2121 <bobbypanwar73@gmail.com>
2023-11-11 23:42:33 +0530

    task-6

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image='https://img.freepik.com/premium-photo/husainabad-clock-tower_78361-2526.jpg?w=2000'
+      title='First Meetup'
+      address='Some Street 5, Some City'
+      description='This is a first meetup'
+    />
+  );
+}
+
+export default MeetupDetails;
